d6471b70e0ee9c525fe3d60f043177731258e93f
nayan-28 <nayanmalakar720@gmail.com>
2025-02-13 17:48:56 +0600

    login api created

--- src/app/api/v1/auth/login/route.js
+++ src/app/api/v1/auth/login/route.js
@@ -12,22 +12,28 @@ export async function POST(req) {
     if (!email || !password) {
         return NextResponse.json({ error: 'Email and password are required.' }, { status: 400 });
     }
+
     const [user] = await query('SELECT * FROM users WHERE email = ?', [email]);
     if (!user) {
         return NextResponse.json({ error: 'User not found.' }, { status: 400 });
     }
+
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
         return NextResponse.json({ error: 'Incorrect password.' }, { status: 400 });
     }
+
     const accessToken = jwt.sign({ id: user.id, email: user.email, username: user.username },
         JWT_SECRET, { expiresIn: JWT_ACCESS_TOKEN_EXPIRATION }
     );
+
     const refreshToken = jwt.sign({ id: user.id, email: user.email },
         JWT_SECRET, { expiresIn: JWT_REFRESH_TOKEN_EXPIRATION }
     );
+
     const headers = {
         'Set-Cookie': `refresh_token=${refreshToken}; HttpOnly; Max-Age=${JWT_REFRESH_TOKEN_EXPIRATION}; Path=/; Secure; SameSite=Strict`,
     };
+
     return NextResponse.json({ accessToken, refreshToken, message: 'Login successful!' }, { status: 200, headers });
 }
