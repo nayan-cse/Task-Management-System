6c9d7b5471cf6dd22b679ec71815b91a3d728541
nayan-28 <nayanmalakar720@gmail.com>
2025-02-14 16:12:45 +0600

    refresh-token optimized

--- src/app/api/v1/auth/refresh-token/route.js
+++ src/app/api/v1/auth/refresh-token/route.js
@@ -1,11 +1,10 @@
 import jwt from 'jsonwebtoken';
 import { NextResponse } from 'next/server';
-
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_ACCESS_TOKEN_EXPIRATION = process.env.JWT_ACCESS_TOKEN_EXPIRATION;
 
+
 export async function POST(req) {
-    // Get the refresh token from the request body (or cookies)
     const { refresh_token } = await req.json();
 
     if (!refresh_token) {
@@ -13,17 +12,12 @@ export async function POST(req) {
     }
 
     try {
-        // Verify the refresh token (decodes the refresh token)
         const decoded = jwt.verify(refresh_token, JWT_SECRET);
 
-        // Generate a new access token using the information from the refresh token
-        const accessToken = jwt.sign({ id: decoded.id, email: decoded.email },
-            JWT_SECRET, { expiresIn: JWT_ACCESS_TOKEN_EXPIRATION } // Access token expiration time (e.g., 1 hour)
-        );
+        const accessToken = jwt.sign({ id: decoded.id, email: decoded.email }, JWT_SECRET, { expiresIn: JWT_ACCESS_TOKEN_EXPIRATION });
 
-        // Return the new access token
         return NextResponse.json({ accessToken, message: 'New access token issued!' }, { status: 200 });
     } catch (err) {
-        return NextResponse.json({ error: 'Invalid or expired refresh token.' }, { status: 400 });
+        return NextResponse.json({ error: 'Invalid or expired refresh token.' }, { status: 401 });
     }
 }
